c8ba8d5dbb1513562e5de2295f01e962f0f47f02
Suraj <surajsangle00@gmail.com>
2025-10-31 20:32:47 +0530

    user data add

--- pages/dailyorders.js
+++ pages/dailyorders.js
@@ -1,10 +1,20 @@
 import DailyOrdersWrapper from "@/components/dailyOrders/dailyOrdersWrapper";
+import { requireValidUser } from "@/utilities/authServer";
 import React from "react";
 
-export default function Dailyorders() {
+export default function Dailyorders({ pageData }) {
   return (
     <>
-      <DailyOrdersWrapper />
+      <DailyOrdersWrapper pageData={pageData} />
     </>
   );
+}
+export async function getServerSideProps(context) {
+  const { user, redirect } = await requireValidUser(context);
+  if (redirect) return { redirect };
+
+  const pageData = {};
+  pageData.user = user;
+
+  return { props: { pageData } };
 }
